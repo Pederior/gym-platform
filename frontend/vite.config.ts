b2f9f1b932332b74b967fe1581bc7ed3fb6761b2
Pederior <ahdreza2000@gmail.com>
2026-02-15 01:50:10 +0330

    fix: backend bugs

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,13 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "http://localhost:5000",
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
